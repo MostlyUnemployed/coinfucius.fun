2e2283102f7790c6acde3559378cc5945c3a902c
Alexander Ramsey <alexanderjohnramsey@gmail.com>
2018-10-30 11:32:12 +0100

    added analytics

--- views/main.js
+++ views/main.js
@@ -95,6 +95,15 @@ function view(state, emit) {
 
   return html `
     <body>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-119810529-5"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-119810529-5');
+    </script>
       <main class=' ${mainStyles} flex flex-column items-center pa4' >
 
         <div class='flex items-center justify-around'>
